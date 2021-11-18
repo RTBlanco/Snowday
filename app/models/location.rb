961d405f2abf0d68e59e91f283f88d8e5b95bcec
RTBlanco <ronnytoribio1@hotmail.com>
2021-11-18 11:00:41 -0500

    added validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,13 @@
 class Location < ApplicationRecord
-  has_many :reports
+  validates :zipcode, presence: true , length: {in: 5..10 }
+
+  has_many :reports,  :dependent => :delete_all
+
+  def highest_reported
+    self.reports.max_by{|report| report.inches}
+  end
+
+  def sorted_reports
+    self.reports.sort_by {|report| report.inches}.reverse
+  end
 end
