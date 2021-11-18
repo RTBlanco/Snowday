20e55ce22b3c4f317ece3e0edb311edf7eb8cd11
RTBlanco <ronnytoribio1@hotmail.com>
2021-11-18 13:04:01 -0500

    wrote test for location model

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "zipcode is required" do 
+    location = Location.create()
+    expect(location).to_not be_valid
+  end
+
+  it "highest_reported returns the largest report" do 
+    
+    location = Location.create(zipcode: "00000")
+    report1 = location.reports.create(inches: "22")
+    report2 = location.reports.create(inches: "2")
+    
+    expect(location.highest_reported).to equal(report1)
+  end
+
+  it "sorted_report return the reports from largest to smallest" do 
+    location = Location.create(zipcode: "00000")
+    report2 = location.reports.create(inches: "2")
+    report1 = location.reports.create(inches: "22")
+
+    expect(location.reload.sorted_reports).to eql([report1, report2])
+  end
+
+  it "should have many reports" do 
+    location = Location.reflect_on_association(:reports)
+    expect(location.macro).to eq(:has_many)
+  end
 end
