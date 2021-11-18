d16590528b191ea845a6f91ed631bd2732c0f50e
RTBlanco <ronnytoribio1@hotmail.com>
2021-11-18 10:57:24 -0500

    add associtation

--- db/migrate/20211118154627_create_reports.rb
+++ db/migrate/20211118154627_create_reports.rb
@@ -2,6 +2,7 @@ class CreateReports < ActiveRecord::Migration[6.1]
   def change
     create_table :reports do |t|
       t.float :inches
+      t.belongs_to :location, foreign_key: true
 
       t.timestamps
     end
